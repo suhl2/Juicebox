da2b3438c3dd50d9440e0a4146998c65c3d7f0e2
Sarah <sarah.celiann@gmail.com>
2023-03-08 11:24:56 -0600

    finished part 2

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { getUserById } = require('../db');
+const { getUserById, createUser, getUserByUsername} = require('../db');
 const { JWT_SECRET } = process.env;
 const express = require('express');
 const apiRouter = express.Router();
@@ -32,6 +32,15 @@ apiRouter.use(async (req, res, next) => {
   }
 });
 
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
+
+
 
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
@@ -41,6 +50,43 @@ apiRouter.use('/posts', postsRouter);
 
 const tagsRouter = require('./tags');
 apiRouter.use('/tags', tagsRouter);
+
+usersRouter.post('/register', async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that username already exists'
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign({ 
+      id: user.id, 
+      username
+    }, process.env.JWT_SECRET, {
+      expiresIn: '1w'
+    });
+
+    res.send({ 
+      message: "thank you for signing up",
+      token 
+    });
+  } catch ({ name, message }) {
+    next({ name, message })
+  } 
+});
+
 
 apiRouter.use((error, req, res, next) => {
     res.send({
--- api/users.js
+++ api/users.js
@@ -33,7 +33,8 @@ usersRouter.post('/login', async (req, res, next) => {
   
       if (user && user.password == password) {
         // create token & return to user
-        res.send({ message: "you're logged in!" });
+        const token = jwt.sign({id: user.id, username: user.username}, process.env.JWT_SECRET);
+        res.send({ message: "you're logged in!", token: token });
       } else {
         next({ 
           name: 'IncorrectCredentialsError', 
