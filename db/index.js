272f6774ac94ec1615d7a75a11d8b48609a3bb0d
Sarah <sarah.celiann@gmail.com>
2023-03-10 19:56:05 -0600

    build script

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const { Client } = require("pg");
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL);
 
 const getAllUsers = async () => {
     const { rows } = await client.query(
